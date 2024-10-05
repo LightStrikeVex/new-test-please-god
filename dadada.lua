89b82e3c96e4eb9e36bb138c6fcc3ef301467a50
LightStrikeVex <151555861+LightStrikeVex@users.noreply.github.com>
2024-10-05 02:55:58 -0300

    Update dadada.lua

--- dadada.lua
+++ dadada.lua
@@ -1,9 +1,9 @@
 local Players = game:GetService("Players")
 local HttpService = game:GetService("HttpService")
-local webhookUrl = "https://discord.com/api/webhooks/1291955749802741812/U1pc46iFRLhoDbx94YNz2f9S_H_yqK8qlxTTFx_JL9uMpRW7zntnHYExYhRmvOxUEfOK"
+local webhookUrl = "https://discord.com/api/webhooks/1292002275899740171/2cupByFKaBMSRKu4ihv3cbtcs-TejHJqnTSeM543KlX1u2VWzq0CxUFJ7p7B5DCQ0fO6"
 
 local function sendToWebhook(playerName, elapsedTime, joinTime, leaveTime, avatarUrl)
-    -- Set default values if any of the variables are nil
+    -- Evitar que el script falle si alguno de los datos es nil
     local safeJoinTime = joinTime or "Unknown join time"
     local safeLeaveTime = leaveTime or "Unknown leave time"
     local safeElapsedTime = elapsedTime or 0
@@ -16,11 +16,19 @@ local function sendToWebhook(playerName, elapsedTime, joinTime, leaveTime, avata
     }
 
     local jsonData = HttpService:JSONEncode(data)
-    HttpService:PostAsync(webhookUrl, jsonData, Enum.HttpContentType.ApplicationJson)
+    
+    -- Enviar datos al webhook y manejar errores
+    local success, response = pcall(function()
+        return HttpService:PostAsync(webhookUrl, jsonData, Enum.HttpContentType.ApplicationJson)
+    end)
+
+    if not success then
+        warn("Error sending to webhook: " .. response) -- Mostrar error en caso de fallo
+    end
 end
 
 local function onPlayerAdded(player)
-    local joinTime = os.date("!%H:%M:%S", os.time()) -- Format to GMT
+    local joinTime = os.date("!%H:%M:%S", os.time())
     local joinTick = tick()
 
     local success, avatarUrl = pcall(function()
@@ -28,14 +36,14 @@ local function onPlayerAdded(player)
     end)
 
     if not success then
-        avatarUrl = ""
+        avatarUrl = "" -- Valor por defecto si no se pudo obtener la imagen
     end
 
     player.AncestryChanged:Connect(function()
-        if player.Parent == nil then
-            local leaveTime = os.date("!%H:%M:%S", os.time()) -- Format to GMT
+        if player.Parent == nil then -- El jugador se ha desconectado
+            local leaveTime = os.date("!%H:%M:%S", os.time())
             local leaveTick = tick()
-            local timeSpent = math.floor((leaveTick - joinTick) / 60)
+            local timeSpent = math.floor((leaveTick - joinTick) / 60) -- Calcular tiempo en minutos
 
             sendToWebhook(player.Name, timeSpent, joinTime, leaveTime, avatarUrl)
         end
